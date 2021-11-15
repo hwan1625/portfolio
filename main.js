27064e760ce66168655c45a982d00999897eb4b2
hwan1625 <57797748+hwan1625@users.noreply.github.com>
2021-11-15 19:50:35 +0900

    JS - arrowUp botton

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 
 //  Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
+const arrowUp = document.querySelector('.arrowUp__btn');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
@@ -10,8 +11,12 @@ document.addEventListener('scroll', () => {
     //console.log(`navabarHeight: ${navbarHeight}`);
     if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
+        // Arrow up button active
+        arrowUp.classList.add('active');
     } else {
         navbar.classList.remove('navbar--dark');
+        // Arrow up button inactive
+        arrowUp.classList.remove('active');
     }
 });
 
@@ -36,3 +41,18 @@ contactMeButton.addEventListener('click', () => {
 function scrollIntoView(selector) {
     document.querySelector(selector).scrollIntoView({behavior : 'smooth'});
 }
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    const scroll = window.scrollY;
+    home.style.opacity = 1 - scroll / homeHeight;
+})
+
+// Click arrowUp button
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+})
+
+
