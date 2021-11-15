68119f6ca3cf707af566f5b078a866c0729888f3
hwan1625 <57797748+hwan1625@users.noreply.github.com>
2021-11-15 21:02:03 +0900

    JS- project filtering

--- main.js
+++ main.js
@@ -55,4 +55,29 @@ arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
 })
 
+// Filtering work categories
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null) {
+        return;
+    }
+    projectContainer.classList.add('anim-out');
+    console.log(filter);
+    setTimeout(() => {
+        projects.forEach((project) => {
+            //console.log(project.dataset.type);
+            if(filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 300)
+})
+
+
 
